f5b38dfa3557a818e23e89a8e2528c281f72b8b9
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-08-26 13:29:47 +0530

    updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useRef } from "react";
+
 const SearchBar = ({query, setQuery}) => {
+
+  let inputElm = useRef(null);
+    useEffect(()=>{
+      document.addEventListener("keydown", (e)=>{
+        if(e.code === "Enter"){
+          inputElm.current.focus();
+        };
+      });
+    }, []);
+
     return <div className="search-bar">
     <input
     className="search"
@@ -6,6 +18,7 @@ const SearchBar = ({query, setQuery}) => {
     placeholder="Search movies..."
     value={query}
     onChange={(e) => setQuery(e.target.value)}
+    ref={inputElm}
   />
 
     </div>
